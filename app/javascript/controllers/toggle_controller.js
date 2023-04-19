20718861d75dd19c9a6cb5694633da01cfea17e7
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-19 03:49:11 +0100

    feat(toggleController): define `listTarget`

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = [ "list" ]
   connect() {
   }
 }
