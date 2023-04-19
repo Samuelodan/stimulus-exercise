c06622d1004bc1159ef4ad4d11f5d7662ad73970
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-19 02:51:40 +0100

    feat: add `demo` routes and set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :demo
   # Defines the root path route ("/")
   # root "articles#index"
+  root "demo#index"
 end
