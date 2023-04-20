c640e0515668bdebc6976ef0b376532fde321717
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-20 03:19:18 +0100

    feat(inputController): define `warnAboutLength` action
    this action is responsible for calculating and displaying the "current length vs length limit" warning in the view

--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -6,4 +6,15 @@ export default class extends Controller {
 
   connect() {
   }
+
+  warnAboutLength() {
+    let lengthLimit = 40;
+    let currentLength = this.fieldTarget.value.length;
+
+    if (currentLength >= (lengthLimit - 5)) {
+      this.displayTarget.textContent = `${currentLength} / ${lengthLimit}`
+    } else {
+      this.displayTarget.textContent = ""
+    }
+  }
 }
