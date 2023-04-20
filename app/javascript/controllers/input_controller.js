b754c2e2851f171ee4c1d1260b4842e8b5c4f998
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-20 03:17:46 +0100

    feat(inputController): define target names

--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="input"
 export default class extends Controller {
+  static targets = [ "display", "field" ]
+
   connect() {
   }
 }
